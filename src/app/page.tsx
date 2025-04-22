3343bd424d18f31fa2d63516a0a13c5e8681aadd
Ing3rulDrogat <myta.boss39@yahoo.com>
2025-04-22 21:16:34 +0100

    Main page layout nearly complete. working on animations next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="flex justify-center items-center w-screen h-screen">
+        <div className="bg-amber-500  lg:h-200 lg:w-250  flex justify-center items-center">
+          <div className="bg-amber-500 text-center h-[90%] w-[90%] flex flex-col">
+            <div className="bg-blue-500 w-full h-1/4 flex justify-center items-center">
+              <div className="bg-red-500 w-1/4 h-full flex"> Front End</div>
+              <div className="bg-red-500 w-1/4 h-full flex"> Back End</div>
+            </div>
+            <div className="bg-blue-500 w-full h-1/4 flex justify-center items-center">
+              <div className="w-full h-full flex">
+                <div className="bg-red-500 w-1/4 h-full "> Work Experience</div>
+                <div className="bg-pink-500 w-1/2 h-full ">
+                  <p className="text-4xl pt-8 motion-preset-typewriter-[24]">
+                    Mihai Trif
+                  </p>
+                  <h1 className="text-4xl">Full Stack</h1>
+                  <h1 className="text-4xl">Software Developer</h1>
+                </div>
+                <div className="bg-red-500 w-1/4 h-full "> Education</div>
+              </div>
+            </div>
+            <div className="bg-blue-500 w-full h-1/4 flex justify-center items-center">
+              <div className="w-full h-full flex">
+                <div className="bg-red-500 w-1/4 h-full "> Projects</div>
+                <div className="bg-pink-500 w-1/2 h-full "> RAAAAAAAAH</div>
+                <div className="bg-red-500 w-1/4 h-full "> About me </div>
+              </div>
+            </div>
+            <div className="bg-blue-500 w-full h-1/4 flex justify-center items-center">
+              <div className="bg-red-500 w-1/4 h-full flex">CV Download</div>
+              <div className="bg-red-500 w-1/4 h-full flex"> ToDo:</div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
